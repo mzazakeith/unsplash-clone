66f34a1213aba0076ddf6f26de29eb3e799eba4e
mzazakeith <mzazakeith@gmail.com>
2018-07-14 23:48:50 +0300

    search functionality implemented

--- gallery/views.py
+++ gallery/views.py
@@ -10,3 +10,16 @@ def index(request):
     return render(request, 'index.html', {"images": images , "title":title})
 
 
+
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get('image')
+        searched_images = Image.search_image(category)
+        message = f"{category}"
+        return render(request, 'search.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {'message': message})
+
+
