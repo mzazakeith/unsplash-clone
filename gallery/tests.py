dda4a27fcd1dff18a7ec9341e3f5c4e871c2028a
mzazakeith <mzazakeith@gmail.com>
2018-07-15 00:02:27 +0300

    category test class

--- gallery/tests.py
+++ gallery/tests.py
@@ -16,3 +16,23 @@ class LocationTestClass(TestCase):
         self.assertTrue(len(locations) > 0)
 
 
+class CategoryTestClass(TestCase):
+
+    def setUp(self):
+        self.abstract = Category(category_name="abstract")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.abstract, Category))
+
+    def test_save_method(self):
+        self.abstract.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_method(self):
+        self.abstract.save_category()
+        self.abstract.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+
+
