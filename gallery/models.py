348b9993878054b31fe76b76698feaff678fe87a
mzazakeith <mzazakeith@gmail.com>
2018-07-14 23:42:05 +0300

    location model made

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+
