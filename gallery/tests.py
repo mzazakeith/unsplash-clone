a8810656ebd7bb73e40f19e0d9eb9830316eceab
mzazakeith <mzazakeith@gmail.com>
2018-07-15 00:02:15 +0300

    Location test class

--- gallery/tests.py
+++ gallery/tests.py
@@ -2,3 +2,17 @@ from django.test import TestCase
 from .models import Image, Category, Location
 
 
+class LocationTestClass(TestCase):
+
+    def setUp(self):
+        self.africa = Location(location='Africa')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.africa, Location))
+
+    def test_save_method(self):
+        self.africa.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+
