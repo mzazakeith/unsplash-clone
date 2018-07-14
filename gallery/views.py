06995967054aed30c4c39097f99a799d08880e89
mzazakeith <mzazakeith@gmail.com>
2018-07-14 23:48:28 +0300

    funtion to display images in the index template(homepage)

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse, Http404
 from gallery.models import Image, Location
 
 
+def index(request):
+    images = Image.show_images()
+    title = "Beautiful Free Images & Pictures | Unsplash"
+    return render(request, 'index.html', {"images": images , "title":title})
+
+
