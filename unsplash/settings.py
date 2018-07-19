699ca760af0f8bcf374be9cd7d56b217f67b4cd5
mzazakeith <mzazakeith@gmail.com>
2018-07-19 20:58:55 +0300

    attempting to fix deploying errors

--- unsplash/settings.py
+++ unsplash/settings.py
@@ -94,7 +94,7 @@ else:
             default=config('DATABASE_URL')
         )
     }
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
